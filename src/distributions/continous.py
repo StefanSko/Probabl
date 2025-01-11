dec449425a9bd2e3a7489b5fa6283f22b0100b2b
StefanSko <Stefan.sko@gmail.com>
2025-01-11 18:09:13 +0100

    add distributions

---
+++ src/distributions/continous.py
@@ -0,0 +1,33 @@
+# src/probflow/distributions/continuous.py
+import jax.numpy as jnp
+from jax.scipy import stats
+
+
+class Distribution:
+    """Base class for probability distributions."""
+
+    def __init__(self, **params):
+        self.parameters = params
+
+    def log_prob(self, value, **params):
+        raise NotImplementedError
+
+
+class Normal(Distribution):
+    """Normal distribution."""
+
+    def __init__(self, loc, scale):
+        super().__init__(loc=loc, scale=scale)
+
+    def log_prob(self, value, loc, scale):
+        return jnp.sum(stats.norm.logpdf(value, loc, scale))
+
+
+class HalfNormal(Distribution):
+    """Half-Normal distribution."""
+
+    def __init__(self, scale):
+        super().__init__(scale=scale)
+
+    def log_prob(self, value, scale):
+        return jnp.sum(stats.halfnorm.logpdf(value, scale=scale))
