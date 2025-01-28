27797d71ea24cabe7fe3a977611362a590cf5119
StefanSko <Stefan.sko@gmail.com>
2025-01-28 15:37:16 +0100

    call lib method for good and remove commented inline code

--- src/normal_prior.py
+++ src/normal_prior.py
@@ -5,7 +5,7 @@ from blackjax.base import State
 from jaxtyping import Array, Float
 
 from distributions.continous import normal
-from inference.samplers import sample_nuts
+from inference.samplers import nuts_with_warmup
 
 # Build the kernel
 step_size = 1e-3
@@ -21,27 +21,8 @@ initial_position = jax.random.normal(rng_key, shape=(1,))
 # Run the sampler
 n_samples = 1000
 initial_state: State = nuts.init(initial_position)
-def inference_loop(
-    rng_key: jax.Array,
-    initial_state: State
-) -> Float[Array, "n_samples 1"]:
-    @jax.jit
-    def one_step(
-        state: State,
-        rng_key: jax.Array
-    ) -> tuple[State, State]:
-        state, _ = nuts.step(rng_key, state)
-        return state, state
 
-    keys = jax.random.split(rng_key, n_samples)
-    final_state, states = jax.lax.scan(one_step, initial_state, keys)
-    return states.position
-
-# Generate samples
-#samples = inference_loop(jax.random.PRNGKey(1), initial_state)
-
-
-samples = sample_nuts(normal(0,1), initial_position, 1000, 42)
+samples = nuts_with_warmup(normal(0,1), initial_position, jax.random.PRNGKey(1000))
 
 empirical_mean = jnp.mean(samples, axis=0)
 empirical_std = jnp.std(samples, axis=0)
