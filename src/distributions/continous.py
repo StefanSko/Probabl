188ec6db3c2c726c2ef82be43763de70af130165
StefanSko <Stefan.sko@gmail.com>
2025-02-07 14:36:53 +0100

    add dataclasses for Parameters; add TypeVar bound to BaseParams; mage ProbabilisticModel generic over TypeVar

--- src/distributions/continous.py
+++ src/distributions/continous.py
@@ -1,14 +1,39 @@
-from typing import Callable, TypeAlias
+from dataclasses import dataclass
+from typing import Callable, Generic, TypeAlias, TypeVar
 
 import jax.numpy as jnp
 from jax.scipy import stats
 from jaxtyping import Array
 
+
+@dataclass(frozen=True)
+class BaseParams:
+    pass
+
+@dataclass(frozen=True)
+class Parameters(BaseParams):
+    params: Array
+
+@dataclass(frozen=True)
+class LocationScaleParams(BaseParams):
+    loc: Array | float
+    scale: Array | float
+
+@dataclass(frozen=True)
+class BetaParams(BaseParams):
+    a: Array | float
+    b: Array | float
+
+@dataclass(frozen=True)
+class GammaParams(BaseParams):
+    shape: Array | float
+    scale: Array | float
+
+P = TypeVar('P', bound=BaseParams)
+
 # Type aliases for Bayesian probability functions
 LogDensityFn: TypeAlias = Callable[[Array], Array]  # log p(x|θ)
-#TODO: FIXME!!
-Parameters = tuple[Array | float, ...]  # θ = (θ₁, θ₂, ...)
-ParametricDensityFn: TypeAlias = Callable[[Parameters], LogDensityFn]  # p(x|θ)
+ParametricDensityFn: TypeAlias = Callable[[P], LogDensityFn]  # p(x|θ)
 LogPDFFn: TypeAlias = Callable[[Array, Array | float, Array | float], Array]  # raw PDF computation
 
 
@@ -18,10 +43,10 @@ Data: TypeAlias = Callable[[], Array]
 
 def make_distribution(
     logpdf_fn: LogPDFFn
-) -> ParametricDensityFn:
-    def distribution(loc: Array | float, scale: Array | float) -> LogDensityFn:
+) -> ParametricDensityFn[LocationScaleParams]:
+    def distribution(params: LocationScaleParams) -> LogDensityFn:
         def log_prob(data: Array) -> Array:
-            return jnp.sum(logpdf_fn(data, loc, scale))
+            return jnp.sum(logpdf_fn(data, params.loc, params.scale))
         return log_prob
     return distribution
 
@@ -45,42 +70,41 @@ def gamma(shape: Array, scale: Array) -> LogDensityFn:
     return log_prob
 
 
-class ProbabilisticModel:
-
-
+class ProbabilisticModel(Generic[P]):
     data: Data
-    parametric_density: ParametricDensityFn
+    parametric_density_fn: ParametricDensityFn[P]
 
-    def __init__(self, data: Data, parametric_density_fn: ParametricDensityFn) -> None:
+    def __init__(self, data: Data, parametric_density_fn: ParametricDensityFn[P]) -> None:
         self.data = data
         self.parametric_density_fn = parametric_density_fn
 
-    def forward(self, params: Array) -> Array:
+    def forward(self, params: P) -> Array:
         return self.parametric_density_fn(params)(self.data())
 
-class ProbabilisticModelBuilder:
-
+class ProbabilisticModelBuilder(Generic[P]):
     data: Data | None
-    parametric_density_fn: ParametricDensityFn | None
+    parametric_density_fn: ParametricDensityFn[P] | None
 
     def __init__(self) -> None:
         self.data = None
         self.parametric_density_fn = None
 
-    def with_data(self, data: Data) -> 'ProbabilisticModelBuilder':
+    def with_data(self, data: Data) -> 'ProbabilisticModelBuilder[P]':
         self.data = data
         return self
 
-    def with_parametric_density_fn(self, parametric_density_fn: ParametricDensityFn) \
-            -> 'ProbabilisticModelBuilder':
+    def with_parametric_density_fn(self, parametric_density_fn: ParametricDensityFn[P]) \
+            -> 'ProbabilisticModelBuilder[P]':
         self.parametric_density_fn = parametric_density_fn
         return self
 
-    def from_model(self, model: ProbabilisticModel) -> 'ProbabilisticModelBuilder':
+    def from_model(self, model: ProbabilisticModel[P]) -> 'ProbabilisticModelBuilder[P]':
         self.data = model.data
         self.parametric_density_fn = model.parametric_density_fn
         return self
 
-    def build(self) -> ProbabilisticModel:
+    def build(self) -> ProbabilisticModel[P]:
+        if self.data is None or self.parametric_density_fn is None:
+            raise ValueError("Both data and parametric_density_fn must be set")
         return ProbabilisticModel(self.data, self.parametric_density_fn)
 
